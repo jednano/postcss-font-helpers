9ed4efc51fa177b2faf6a29eb7c440596bf6e033
Jed Mao <jedmao@outlook.com>
2015-09-29 16:08:25 -0500

    v2.0.0: Use tcomb-postcss to enforce PostCSS types

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 2.0.0
+- Use [`tcomb-postcss](https://github.com/jedmao/tcomb-postcss) to enforce PostCSS types.
+
 ## 1.0.1
 - Move PostCSS dependency to dev dependencies.
 
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var resolveProp = require('postcss-resolve-prop');
 var parseCssFont = require('parse-css-font');
 var unquote = require('unquote');
+var t = require('tcomb-postcss');
 
 [
 	'style',
@@ -9,43 +10,49 @@ var unquote = require('unquote');
 	'stretch',
 	'size'
 ].forEach(function(prop) {
-	module.exports[prop] = function(rule) {
-		return resolveProp(rule, 'font-' + prop, {
+	module.exports[prop] = t.func(t.Container, t.Any).of(
+		function(container) {
+			return resolveProp(container, 'font-' + prop, {
+				parsers: {
+					font: function(value) {
+						return parseCssFont(value)[prop];
+					}
+				}
+			});
+		}
+	);
+});
+
+module.exports.family = t.func(t.Container, t.Any).of(
+	function(rule) {
+		return resolveProp(rule, 'font-family', {
 			parsers: {
 				font: function(value) {
-					return parseCssFont(value)[prop];
+					return parseCssFont(value).family;
+				},
+				'font-family': function(value) {
+					return value.split(/\s*,\s*/).map(unquote);
 				}
 			}
 		});
-	};
-});
+	}
+);
 
-module.exports.family = function(rule) {
-	return resolveProp(rule, 'font-family', {
-		parsers: {
-			font: function(value) {
-				return parseCssFont(value).family;
-			},
-			'font-family': function(value) {
-				return value.split(/\s*,\s*/).map(unquote);
-			}
-		}
-	});
-}
-
-module.exports.lineHeight = function(rule) {
-	return resolveProp(rule, 'line-height', {
-		parsers: {
-			font: function(value) {
-				return parseCssFont(value).lineHeight;
-			},
-			'line-height': function(value) {
-				var parsed = parseFloat(value);
-				if (parsed.toString() === value) {
-					return parsed;
+module.exports.lineHeight = t.func(t.Container, t.Any).of(
+	function(rule) {
+		return resolveProp(rule, 'line-height', {
+			parsers: {
+				font: function(value) {
+					return parseCssFont(value).lineHeight;
+				},
+				'line-height': function(value) {
+					var parsed = parseFloat(value);
+					if (parsed.toString() === value) {
+						return parsed;
+					}
+					return value;
 				}
-				return value;
 			}
-		}
-	});
-}
+		});
+	}
+);
